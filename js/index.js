b6e9f226383e4c438dbb9391312249c5085586b4
cabralcaio <caio_cabral1@hotmail.com>
2022-09-30 16:06:58 -0300

    Aula 30/09

---
+++ js/index.js
@@ -0,0 +1,3 @@
+import usuarios from "./moduloDados.js";
+//trouxe o arquivo array do moduloDados para esse arquivo JS
+console.log(usuarios)
---
+++ js/moduloDados.js
@@ -0,0 +1,30 @@
+const usuarios = [
+    {
+        id: 1,
+        nomeUsuario: "caiocabral",
+        senha: "1234",
+        email: "caio@hotmail.com"
+    },
+    {
+        id: 2,
+        nomeUsuario: "jose-almir",
+        senha: "1234",
+        email: "jose@hotmail.com"
+    },
+    {
+        id: 3,
+        nomeUsuario: "renato",
+        senha: "1234",
+        email: "renato@hotmail.com"
+    },
+    {
+        id: 4,
+        nomeUsuario: "leticiacabral",
+        senha: "1234",
+        email: "caio@hotmail.com"
+    }
+]
+
+export default usuarios;
+
+//disponibiliza o array  para ser utilizado fora do arquivo moduloDados.js
